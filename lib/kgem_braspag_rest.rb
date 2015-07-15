e4c1fb34d705c8819618f7fd74d3ab0be6d403e7
Rafael Moraes <rafael@komeia.com>
2015-07-15 17:58:33 -0300

    alterado configurações default

--- lib/kgem_braspag_rest.rb
+++ lib/kgem_braspag_rest.rb
@@ -81,17 +81,22 @@ module KBraspag
   ## PAYMENT METHODS AVAILABLE
   define_setting :payment_types, {
                                   :credit_card => "CreditCard",
-                                  :debit_card => "DebitCard",
-                                  :eletronic_transfer => "EletronicTransfer",
+                                  # :debit_card => "DebitCard",
+                                  # :eletronic_transfer => "EletronicTransfer",
                                   :payment_slip => "Boleto"
                                  }
 
   ## CREDIT CARD BRANDS AVAILABLE
   define_setting :brands, {
-                            :visa => 'Visa', :mastercard => 'Master',
-                            :amex => 'Amex', :elo => 'Elo', :aura => 'Aura',
-                            :jcb => 'JCB', :diners => 'Diners',
-                            :discover => 'Discover', :hipercard => 'Hipercard',
+                            :visa => 'Visa',
+                            :mastercard => 'Master',
+                            :amex => 'Amex',
+                            :elo => 'Elo',
+                            :aura => 'Aura',
+                            :jcb => 'JCB',
+                            :diners => 'Diners',
+                            :discover => 'Discover',
+                            :hipercard => 'Hipercard',
                             :hiper => 'Hiper'
                           }
 
@@ -101,7 +106,7 @@ module KBraspag
                                            :cielo => "Cielo",
                                            :rede => "Redecard"
                                          }
-  define_setting :debit_providers, {:cielo => "Cielo"}
+  # define_setting :debit_providers, {:cielo => "Cielo"}
   define_setting :payment_slip_providers, {
                                             :bradesco => "Bradesco",
                                             :banco_do_brasil => "BancoDoBrasil",
@@ -113,24 +118,25 @@ module KBraspag
                                             :hsbc => "HSBC",
                                             :simulado => "Simulado"
                                           }
-  define_setting :eletronic_transfer_providers, {
-                                                  :bradesco => "Bradesco",
-                                                  :simulado => "Simulado"
-                                                }
+  # define_setting :eletronic_transfer_providers, {
+  #                                                 :bradesco => "Bradesco",
+  #                                                 :simulado => "Simulado"
+  #                                               }
 
   ##REQUEST SETTINGS
   define_setting :timeout, 15
-  define_setting :merchant_id, "e895fcba-14df-49f6-a019-e576c136f520"
-  define_setting :merchant_key, "FLKAVCQRLZKEZFFSDOYDBXPMWSUYWFSCMGDNKYYJ"
-  # define_setting :merchant_id, "4E9083D8-6584-E311-93FD-000AF7120B63"
-  # define_setting :merchant_key, "LRGS34zfF06c8lilS2hJRmJFiMw8fawd0HIIDtHZ"
+  # Development merchant
+  # define_setting :merchant_id, "e895fcba-14df-49f6-a019-e576c136f520"
+  # define_setting :merchant_key, "FLKAVCQRLZKEZFFSDOYDBXPMWSUYWFSCMGDNKYYJ"
   define_setting :connection_attempts, 3
 
   ##URLs
-  define_setting :payment_url, "https://apisandbox.braspag.com.br"
-  define_setting :query_url, "https://apiquerysandbox.braspag.com.br"
-  # define_setting :payment_url, "https://api.braspag.com.br"
-  # define_setting :query_url, "https://apiquery.braspag.com.br"
+  # define_setting :payment_url, "https://apisandbox.braspag.com.br"
+  # define_setting :query_url, "https://apiquerysandbox.braspag.com.br"
+
+  # Production URLs
+  define_setting :payment_url, "https://api.braspag.com.br"
+  define_setting :query_url, "https://apiquery.braspag.com.br"
 
   ##RESOURCES
   define_setting :complete_payment_slip_resource, "/v2/sales/"
